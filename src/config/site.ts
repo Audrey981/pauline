99a47282398f909468a429e3eb5ee5db5e91ea96
Audrey981 <blondela1215@gmail.com>
2026-06-23 16:05:43 +0200

    travail sur charte graphique

---
+++ src/config/site.ts
@@ -0,0 +1,27 @@
+// src/config/site.ts
+export const siteConfig = {
+  name: "Natacha Boxe Thérapie",
+  url: "https://paulinerebufat.fr.fr",
+  tagline: "Céramiste dans le Pays de retz",
+  description: "Pauline, céramiste et potière expérimentée. Atelier, cours et exposition dans le Pays de Retz",
+  defaultOgImage: "https://www.natacha-boxetherapie.fr/natacha1.jpeg",
+  telephone: "+33660828775",
+  email: "pauline.rebufat@gmail.com",
+  facebook: "https://www.facebook.com/CERAMIQUEPAULINEREBUFAT",
+  themeColor: "#9aa882",
+  adresse: {
+    rue: "",
+    ville: "Saint Père en Retz",
+    codePostal: "44320",
+    pays: "FR"
+  },
+  geo: {
+    latitude: 47.200001,                                   
+    longitude: -2.033330
+  },
+  horaires: {
+    jours: ["Monday","Tuesday","Wednesday","Thursday","Friday"],
+    ouverture: "09:00",
+    fermeture: "19:00"
+  }
+}
