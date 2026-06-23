3bae2f67059da87f03ccae4f418a404bf0bc65b7
Audrey981 <blondela1215@gmail.com>
2026-06-23 17:06:45 +0200

    Configure Astro deploy on GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://www.natacha-boxetherapie.fr',
-  // base: '/Natacha-Boxe-Therapie',
+  site: 'https://paulineatelierceramique-commits.github.io',
+  base: '/pauline',
   integrations: [sitemap()],
 });
+
+
+
+
+
+
+
+// à enlever quand le site sera en ligne sur le domaine définitif
+// @ts-check
+//import { defineConfig } from 'astro/config';
+
+// import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+//export default defineConfig({
+ // site: 'https://paulinerebufat.fr',
+  // base: '/pauline',
+ // integrations: [sitemap()],
+// });
